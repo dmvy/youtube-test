f7ddd07a4559978d8b1668f3f3f41b7ccb65cd7d
Ilya Shchekalev <git@dmvy.ru>
2013-05-08 09:15:21 +0600

    Updated youtube-dl. Now using binary version.

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ for video in `cat video`; do
 		touch $video/speed_360.log
 	fi
 	# get 360p
-	q360_url="`./youtube-dl.py --get-url -f 34 "http://www.youtube.com/watch?v=$video"`"
+	q360_url="`./youtube-dl --get-url -f 34 "http://www.youtube.com/watch?v=$video"`"
 	/usr/bin/wget --progress=dot:mega -o $video/wget.log -O /dev/null "$q360_url"
 	grep saved $video/wget.log >> $video/speed_360.log
 	HOST360=`cat $video/wget.log | grep "|" | cut -d "|" -f 2|tail -n 1`
@@ -19,7 +19,7 @@ for video in `cat video`; do
 	/usr/bin/mtr -wrc100 $HOST360 > $video/${DT}_360.mtr &
 
 	# get 720p
-	q720_url="`./youtube-dl.py --get-url -f 22 "http://www.youtube.com/watch?v=$video"`"
+	q720_url="`./youtube-dl --get-url -f 22 "http://www.youtube.com/watch?v=$video"`"
 	/usr/bin/wget --progress=dot:mega -o $video/wget.log -O /dev/null "$q720_url"
 	grep saved $video/wget.log >> $video/speed_720.log
 	HOST720=`cat $video/wget.log | grep "|" | cut -d "|" -f 2|tail -n 1`
--- youtube-dl.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import sys
-sys.path = sys.path + ['youtube-dl']
-import __init__ 
-
-if __name__ == '__main__':
-	__init__.main()
